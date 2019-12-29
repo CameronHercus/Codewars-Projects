d5a0c0ae4b795da9b6f455d431efbc2c2c803f7c
Cherub2K <48897247>
2019-12-29 15:38:12 +1300

    Get the Middle Character

---
+++ c#/Get the Middle Character/Get the Middle Character/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Get_the_Middle_Character
+{
+    public class Kata
+    {
+        public static string GetMiddle(string s)
+        {
+            List<char> charList = new List<char>();
+            if (string.IsNullOrEmpty(s) || s.Length == 0)
+            {
+                return "";
+            }
+            if (s.Length % 2 ==0)
+            {
+                charList.Add(s[(s.Length / 2) - 1]);
+                charList.Add(s[s.Length / 2]);
+            }
+            else
+            {
+                charList.Add(s[(s.Length-1) / 2]);
+            }
+            return String.Join("", charList.ToArray());
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.GetMiddle("Hello"));
+            Console.WriteLine(Kata.GetMiddle(""));
+            Console.WriteLine(Kata.GetMiddle("He"));
+            Console.WriteLine(Kata.GetMiddle("H"));
+        }
+    }
+}
