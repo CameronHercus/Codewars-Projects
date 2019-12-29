58e630f9260ab1f3265478a6a1dff50f6a8a8a8e
Cherub2K <48897247>
2019-12-29 19:22:35 +1300

    Your order, please

---
+++ c#/Your order, please/Your order, please/Program.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Your_order__please
+{
+    public static class Kata
+    {
+
+        public static string Order(string words)
+        {
+            string[] wordsArray = words.Split(" ");
+            string[] stringsArray = new String[wordsArray.Length];
+            foreach (string i in wordsArray)
+            {
+                foreach (char j in i)
+                {
+                    if (Char.IsDigit(j))
+                    {
+                        stringsArray[((int)Char.GetNumericValue(j)) - 1] = i;
+                    }
+                }
+            }
+            return string.Join(" ", stringsArray);
+        }
+        static void Main(string[] args)
+        {
+            Assert.AreEqual("Thi1s is2 3a T4est", Kata.Order("is2 Thi1s T4est 3a"));
+        }
+    }
+}
