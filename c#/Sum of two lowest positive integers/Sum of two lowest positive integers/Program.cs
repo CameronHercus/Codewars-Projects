4bc0afc57a075871fbff2b0a8fe8cef3d7670f31
Cherub2K <48897247>
2019-12-31 17:24:48 +1300

    sum of lowest two nums in array

---
+++ c#/Sum of two lowest positive integers/Sum of two lowest positive integers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Sum_of_two_lowest_positive_integers
+{
+    public static class Kata
+    {
+        public static int sumTwoSmallestNumbers(int[] numbers)
+        {
+            int first = int.MaxValue;
+            int second = int.MaxValue;
+            foreach (int i in numbers)
+            {
+                if (i < first)
+                {
+                    second = first;
+                    first = i;
+                }
+                else if (i < second && i != first)
+                {
+                    second = i;
+                }
+            }
+            return first + second;
+        }
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 4, 12, 19, 22 };
+            Console.WriteLine(Kata.sumTwoSmallestNumbers(numbers));
+        }
+    }
+}
