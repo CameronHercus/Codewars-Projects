a45696b30b28967ad788ca19451c6e850328b75c
CameronHercus <cameron.hercus@gmail.com>
2020-01-02 12:32:29 +1300

    exes and ohs

---
+++ c#/Exes and Ohs/Exes and Ohs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exes_and_Ohs
+{
+    public static class Kata
+    {
+        public static bool XO(string input)
+        {
+            int oCounter = 0;
+            int xCounter = 0;
+            foreach (char s in input)
+            {
+                if (s == 'x' || s =='X') {
+                    xCounter += 1;
+                }
+                else if (s == 'o' || s == 'O')
+                {
+                    oCounter += 1;
+                }
+            }
+            if (oCounter == xCounter)
+            {
+                return true;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.XO("xxOo"));
+        }
+    }
+}
