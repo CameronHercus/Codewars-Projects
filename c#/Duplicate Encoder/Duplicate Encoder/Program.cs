f3f8b73a2391fb2c2b8389090c5263e0ae22e08d
CameronHercus <cameron.hercus@gmail.com>
2020-01-02 16:25:00 +1300

    Duplicate Encoder

---
+++ c#/Duplicate Encoder/Duplicate Encoder/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Duplicate_Encoder
+{
+    public class Kata
+    {
+        public static string DuplicateEncode(string word)
+        {
+            string wordLower = word.ToLower();
+            Dictionary<char, char> dict = new Dictionary<char, char>();
+            StringBuilder myString = new StringBuilder();
+            foreach (char c in wordLower)
+            {
+                if (dict.ContainsKey(c))
+                {
+                    dict[c] = ')';
+                }
+                else
+                {
+                    dict.Add(c, '(');
+                }
+            }
+            foreach (char c in wordLower)
+            {
+                myString.Append(dict[c]);
+            }
+            
+            return myString.ToString();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.DuplicateEncode("recede"));
+        }
+    }
+}
