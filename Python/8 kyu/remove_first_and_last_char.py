1b1d6eee5c784480f26fc13a741d995f89e363dd
Cam <Cameron.Hercus@gmail.com>
2022-03-22 16:39:32 +1300

    22/03/2022
    practice

---
+++ Python/8 kyu/remove_first_and_last_char.py
@@ -0,0 +1,9 @@
+"""
+8 kyu - Remove First and Last Character
+"""
+
+
+
+def remove_char(s):
+    """8 kyu - Remove First and Last Character"""
+    return s[1:-1]
---
+++ Python/8 kyu/remove_string_spaces.py
@@ -0,0 +1,7 @@
+"""
+8 kyu - Remove String Spaces
+"""
+
+def no_space(x):
+    """8 kyu - Remove String Spaces"""
+    return x.replace(" ", "")
