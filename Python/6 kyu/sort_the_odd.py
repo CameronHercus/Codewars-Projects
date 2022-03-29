045fa6c29f7183d6f675d24e61b38c391ae73bf1
Cam <Cameron.Hercus@gmail.com>
2022-03-30 11:48:20 +1300

    30/03/2022
    practice

---
+++ Python/6 kyu/sort_the_odd.py
@@ -0,0 +1,17 @@
+"""
+6 kyu - Sort the odd
+"""
+
+
+def sort_array(source_array):
+    """Return a sorted array."""
+    odd_array = [i for i in source_array if i %2==1]
+    odd_array.sort()
+    counter = 0
+    for j in range(len(source_array)):
+        if source_array[j] % 2 == 1:
+            source_array[j] = odd_array[counter]
+            counter += 1
+    return source_array
+
+print((sort_array([5, 3, 2, 8, 1, 4])))
--- Python/7 kyu/alphabetical_grid.py
+++ Python/7 kyu/alphabetical_grid.py
@@ -6,21 +6,9 @@ import string
 
 def grid(N):
     """7 kyu - Alphabetical Grid"""
-    if N < 0:
-        return None
-    result = string.ascii_lowercase
-    blank = []
-    final = []
-    counter = 0
-    for i in range(N):
-        counter = i
-        for j in range(N):
-            if counter > 25:
-                counter = counter % 26
-            blank.append(result[counter])
-            counter += 1
-        final.append(" ".join(blank))
-        blank = []
-    return "\n".join(final)
+    if N >= 0:
+        alpha_list = string.ascii_lowercase
+        return "\n".join(" ".join([alpha_list[j%26] for j in range(i, N+i)]) for i in range(N))
+    return None
 
 print(grid(30))
