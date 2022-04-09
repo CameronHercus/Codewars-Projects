0553e3997a81c7222ce515b50765320a2f19e3a9
Cam <Cameron.Hercus@gmail.com>
2022-04-09 15:14:01 +1200

    9/04/2022
    practice

---
+++ Python/6 kyu/playing_with_passphrases.py
@@ -0,0 +1,31 @@
+import string
+def play_pass(s, n):
+    """work pls"""
+    lower_list = string.ascii_lowercase
+    upper_list = string.ascii_uppercase
+    final = []
+    for i, j in enumerate(list(s)):
+        if j.isalpha() is True:
+            if i % 2 == 1:
+                final.append(lower_list[(lower_list.index(j.lower()) + n) % 26])
+                print((lower_list.index(j.lower()) + n) % 26)
+            else:
+                final.append(upper_list[(upper_list.index(j.upper()) + n) % 26])
+                print((upper_list.index(j.upper()) + n) % 26)
+        elif j.isdigit():
+            final.append(str(complement(int(j))))
+        else:
+            final.append(j)
+    return "".join(reversed(final))
+
+
+def complement(number):
+    for i in range(9):
+        if i + number == 9:
+            return i
+    return 9
+print(play_pass("xy", 1))
+
+print(complement(9))
+print(complement(7))
+print(complement(0))
